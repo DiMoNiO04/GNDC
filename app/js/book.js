71fcfb29ce6c589696a2e4b6208ea4e2b7ccd28f
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-06-09 13:06:20 +0300

    refactor: book first page and main index

--- app/js/book.js
+++ app/js/book.js
@@ -5,7 +5,6 @@ function updateDepth(book, newPage) {
 	var page = book.turn('page'),
 		pages = book.turn('pages'),
 		depthWidth = 16*Math.min(1, page*2/pages);
-
 		newPage = newPage || page;
 
 	if (newPage>2)
@@ -29,7 +28,6 @@ function updateDepth(book, newPage) {
 }
 
 function loadPage(page) {
-
 	$.ajax({url: 'pages/page' + page + '.html'}).
 		done(function(pageHtml) {
 			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
@@ -57,15 +55,11 @@ function addPage(page, book) {
 }
 
 function numberOfViews(book) {
-
 	return book.turn('pages') / 2 + 1;
-
 }
 
 function getViewNumber(book, page) {
-
 	return parseInt((page || book.turn('page'))/2 + 1, 10);
-
 }
 
 function zoomHandle(e) {
@@ -189,43 +183,43 @@ function moveBar(yes) {
 	}
 }
 
-function setPreview(view) {
+// function setPreview(view) {
 
-	var previewWidth = 115,
-		previewHeight = 73,
-		previewSrc = 'pages/preview.jpg',
-		preview = $(_thumbPreview.children(':first')),
-		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
-		width = (numPages==1) ? previewWidth/2 : previewWidth;
+// 	var previewWidth = 115,
+// 		previewHeight = 73,
+// 		previewSrc = 'pages/preview.jpg',
+// 		preview = $(_thumbPreview.children(':first')),
+// 		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
+// 		width = (numPages==1) ? previewWidth/2 : previewWidth;
 
-	_thumbPreview.
-		addClass('no-transition').
-		css({width: width + 15,
-			height: previewHeight + 15,
-			top: -previewHeight - 30,
-			left: ($($('#slider').children(':first')).width() - width - 15)/2
-		});
+// 	_thumbPreview.
+// 		addClass('no-transition').
+// 		css({width: width + 15,
+// 			height: previewHeight + 15,
+// 			top: -previewHeight - 30,
+// 			left: ($($('#slider').children(':first')).width() - width - 15)/2
+// 		});
 
-	preview.css({
-		width: width,
-		height: previewHeight
-	});
+// 	preview.css({
+// 		width: width,
+// 		height: previewHeight
+// 	});
 
-	if (preview.css('background-image')==='' ||
-		preview.css('background-image')=='none') {
+// 	if (preview.css('background-image')==='' ||
+// 		preview.css('background-image')=='none') {
 
-		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
+// 		preview.css({backgroundImage: 'url(' + previewSrc + ')'});
 
-		setTimeout(function(){
-			_thumbPreview.removeClass('no-transition');
-		}, 0);
+// 		setTimeout(function(){
+// 			_thumbPreview.removeClass('no-transition');
+// 		}, 0);
 
-	}
+// 	}
 
-	preview.css({backgroundPosition:
-		'0px -'+((view-1)*previewHeight)+'rem'
-	});
-}
+// 	preview.css({backgroundPosition:
+// 		'0px -'+((view-1)*previewHeight)+'rem'
+// 	});
+// }
 
 function isChrome() {
 

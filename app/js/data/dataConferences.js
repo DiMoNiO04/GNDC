99cdc112fa3c759ea03fcca28fa869ce570667ad
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-06-21 20:42:48 +0300

    feat: add map and conferences popup

---
+++ app/js/data/dataConferences.js
@@ -0,0 +1,211 @@
+export const dataConferences = {
+	
+	tula: {
+		title: '17 февраля — г. Тула',
+		imgSrc: [
+			'./img/conferences/tula/tula-3.webp',
+			'./img/conferences/tula/tula-1.webp',
+			'./img/conferences/tula/tula-2.webp',
+			'./img/conferences/tula/tula-3.webp',
+			'./img/conferences/tula/tula-1.webp',
+		],
+		imgAlt: 'Тула',
+		info: 'Информация о конференции в Туле',
+		program: 'Информация о программе в Туле',
+		results: 'Информация о результатах в Туле',
+		photos: 'Информация о фотогалерии в Туле'
+	},
+
+	samara: {
+		title: '17 марта — г. Самара',
+		imgSrc: [
+			'./img/conferences/samara/samara-3.webp',
+			'./img/conferences/samara/samara-1.webp',
+			'./img/conferences/samara/samara-2.webp',
+			'./img/conferences/samara/samara-3.webp',
+			'./img/conferences/samara/samara-1.webp',
+		],
+		imgAlt: 'Самара',
+		info: 'Информация о конференции в Самаре',
+		program: 'Информация о програме в Самаре',
+		results: 'Информация о результатах в Самаре',
+		photos: 'Информация о фотогалерии в Самаре'
+	},
+
+	vladikavkaz: {
+		title: '7 апреля — г. Владикавказ',
+		imgSrc: [
+			'./img/conferences/vladikavkaz/vladikavkaz-3.webp',
+			'./img/conferences/vladikavkaz/vladikavkaz-1.webp',
+			'./img/conferences/vladikavkaz/vladikavkaz-2.webp',
+			'./img/conferences/vladikavkaz/vladikavkaz-3.webp',
+			'./img/conferences/vladikavkaz/vladikavkaz-1.webp',
+		],
+		imgAlt: 'Владикавказ',
+		info: 'Информация о конференции в Вадикавказе',
+		program: 'Информация о программе в Владикавказе',
+		results: 'Информация о результатах в Владикавказе',
+		photos: 'Информация о фотогалерии в Владикавказе'
+	},
+
+	voronezh: {
+		title: '21 апреля — г. Воронеж',
+		imgSrc: [
+			'./img/conferences/voronezh/voronezh-3.webp',
+			'./img/conferences/voronezh/voronezh-1.webp',
+			'./img/conferences/voronezh/voronezh-2.webp',
+			'./img/conferences/voronezh/voronezh-3.webp',
+			'./img/conferences/voronezh/voronezh-1.webp',
+		],
+		imgAlt: 'Воронеж',
+		info: 'Информация о конференции в Воронеже',
+		program: 'Информация о програме в Воронеже',
+		results: 'Информация о резульатах в Воронеже',
+		photos: 'Информация о фотогалерии в Воронеже'
+	},
+
+	moscow: {
+		title: '12 мая — г. Москва',
+		imgSrc: [
+			'./img/conferences/moscow1/moscow1-3.webp',
+			'./img/conferences/moscow1/moscow1-1.webp',
+			'./img/conferences/moscow1/moscow1-2.webp',
+			'./img/conferences/moscow1/moscow1-3.webp',
+			'./img/conferences/moscow1/moscow1-1.webp',
+		],
+		imgAlt: 'Москва',
+		info: 'Информация о конференции в Москве',
+		program: 'Информация о програме в Москве',
+		results: 'Информация о резульатах в Москве',
+		photos: 'Информация о фотогалерии в Москве'
+	},
+
+	saratov: {
+		title: '25-26 мая — г. саратов',
+		imgSrc: [
+			'./img/conferences/saratov/saratov-3.webp',
+			'./img/conferences/saratov/saratov-1.webp',
+			'./img/conferences/saratov/saratov-2.webp',
+			'./img/conferences/saratov/saratov-3.webp',
+			'./img/conferences/saratov/saratov-1.webp',
+		],
+		imgAlt: 'Саратов',
+		info: 'Информация о конференции в Саратове',
+		program: 'Информация о програме в Саратове',
+		results: 'Информация о результатах в Саратове',
+		photos: 'Информация о фотогалерии в Саратове'
+	},
+
+	volgograd: {
+		title: '15-16 июня — г. Волгоград',
+		imgSrc: [
+			'./img/conferences/volgograd/volgograd-3.webp',
+			'./img/conferences/volgograd/volgograd-1.webp',
+			'./img/conferences/volgograd/volgograd-2.webp',
+			'./img/conferences/volgograd/volgograd-3.webp',
+			'./img/conferences/volgograd/volgograd-1.webp',
+		],
+		imgAlt: 'Волгоград',
+		info: 'Информация о конференции в Волгограде',
+		program: 'Информация о програме в Волгограде',
+		results: 'Информация о результатах в Волгограде',
+		photos: 'Информация о фотогалерии в Волгограду'
+	},
+
+	penza: {
+		title: '8 сентября — г. Пенза',
+		imgSrc: [
+			'./img/conferences/penza/penza-3.webp',
+			'./img/conferences/penza/penza-1.webp',
+			'./img/conferences/penza/penza-2.webp',
+			'./img/conferences/penza/penza-3.webp',
+			'./img/conferences/penza/penza-1.webp',
+		],
+		imgAlt: 'Пенза',
+		info: 'Информация о конференции в Пензе',
+		program: 'Информация о програме в Пензе',
+		results: 'Информация о результатах в Пензе',
+		photos: 'Информация о фотогалерии в Пензе'
+	},
+	
+	moscow2: {
+		title: '19-22 сентября — г. Москва',
+		imgSrc: [
+			'./img/conferences/moscow2/moscow2-3.webp',
+			'./img/conferences/moscow2/moscow2-1.webp',
+			'./img/conferences/moscow2/moscow2-2.webp',
+			'./img/conferences/moscow2/moscow2-3.webp',
+			'./img/conferences/moscow2/moscow2-1.webp',
+		],
+		imgAlt: 'Москва',
+		info: 'Информация о конференции в Москве',
+		program: 'Информация о програме в Москве',
+		results: 'Информация о результатах в Москве',
+		photos: 'Информация о фотогалерии в Москве'
+	},
+
+	sevastopol: {
+		title: '29 сентября — г. Севастополь',
+		imgSrc: [
+			'./img/conferences/sevastopol/sevastopol-3.webp',
+			'./img/conferences/sevastopol/sevastopol-1.webp',
+			'./img/conferences/sevastopol/sevastopol-2.webp',
+			'./img/conferences/sevastopol/sevastopol-3.webp',
+			'./img/conferences/sevastopol/sevastopol-1.webp',
+		],
+		imgAlt: 'Севастополь',
+		info: 'Информация о конференции в Севастополе',
+		program: 'Информация о програме в Севастополе',
+		results: 'Информация о результатах в Севастополе',
+		photos: 'Информация о фотогалерии в Севастополе'
+	},
+
+	novosibirsk: {
+		title: '12-13 октября —  г. Новосибирск',
+		imgSrc: [
+			'./img/conferences/novosibirsk/novosibirsk-3.webp',
+			'./img/conferences/novosibirsk/novosibirsk-1.webp',
+			'./img/conferences/novosibirsk/novosibirsk-2.webp',
+			'./img/conferences/novosibirsk/novosibirsk-3.webp',
+			'./img/conferences/novosibirsk/novosibirsk-1.webp',
+		],
+		imgAlt: 'Новосибирск',
+		info: 'Информация о конференции в Новосибирске',
+		program: 'Информация о програме в Новосибирске',
+		results: 'Информация о результатах в Новосибирске',
+		photos: 'Информация о фотогалерии в Нвосибирске'
+	},
+
+	peterburg: {
+		title: '26-28 октября — г. Санкт-Петербург',
+		imgSrc: [
+			'./img/conferences/peterburg/peterburg-3.webp',
+			'./img/conferences/peterburg/peterburg-1.webp',
+			'./img/conferences/peterburg/peterburg-2.webp',
+			'./img/conferences/peterburg/peterburg-3.webp',
+			'./img/conferences/peterburg/peterburg-1.webp',
+		],
+		imgAlt: 'Санкт-Петербург',
+		info: 'Информация о конференции в Санкт-Петербурге',
+		program: 'Информация о програме в Санкт-Петербурге',
+		results: 'Информация о результатах в Санкт-Петербурге',
+		photos: 'Информация о фотогалерии в Санкт-Петербурге'
+	},
+
+	kazan: {
+		title: '23-24 ноября — г. Казань',
+		imgSrc: [
+			'./img/conferences/kazan/kazan-3.webp',
+			'./img/conferences/kazan/kazan-1.webp',
+			'./img/conferences/kazan/kazan-2.webp',
+			'./img/conferences/kazan/kazan-3.webp',
+			'./img/conferences/kazan/kazan-1.webp',
+		],
+		imgAlt: 'Казань',
+		info: 'Информация о конференции в Казане',
+		program: 'Информация о програме в Казане',
+		results: 'Информация о результатах в Кзаане',
+		photos: 'Информация о фотогалерии в Казане'
+	}
+
+}

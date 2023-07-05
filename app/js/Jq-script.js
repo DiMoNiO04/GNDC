6e164517f01ecd4619e84476161db3fcb3732ccd
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-07-05 17:30:17 +0300

    refactor: add about page

--- app/js/Jq-script.js
+++ app/js/Jq-script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 
-
 $(document).ready(function () {
     $('.input-file input[type=file]').on('change', function(){
         let file = this.files[0];
@@ -9,7 +8,6 @@ $(document).ready(function () {
     });
 
     // инпут для скачивания файла 
-
 
     $('.select').on('click', '.select-head', function () {
         if ($(this).hasClass('open')) {
@@ -36,18 +34,6 @@ $(document).ready(function () {
             $('.select-list').fadeOut();
         }
 });
-    
-
-$(".cost__block").each(function() {
-    let more = $(this).find(".cost__block-inner");
-    let hide = $(this).find(".cost__content");
-    hide.hide();
-    more.click(function() {
-        hide.slideToggle();
-        more.toggleClass('active');
-    });
-});
-
 
 
 $(".block__item").each(function() {
@@ -61,109 +47,6 @@ $(".block__item").each(function() {
 });
 
 
-
-$(".schedule__block").each(function() {
-    let more = $(this).find(".schedule__block-inner");
-    let hide = $(this).find(".schedule__wrap");
-    hide.hide();
-    more.click(function() {
-        hide.slideToggle();
-        more.toggleClass('active');
-    });
-});
-
-
-
-
-
-$('.hospitalization__item').click(function() {
-	var id = $(this).attr('data-tab'),
-			content = $('.hospitalization__content[data-tab="'+ id +'"]');
-	
-	$('.hospitalization__item.active').removeClass('active'); // 1
-	$(this).addClass('active'); // 2
-	
-	$('.hospitalization__content.active').removeClass('active'); // 3
-	content.addClass('active'); // 4
-
-	if(id == 1) {
-		$('.hospitalization__block').removeClass('hidden'); // 1
-	} else {
-		$('.hospitalization__block').addClass('hidden'); // 1
-	}
-});
-});
-
-
-//--------------Connecttion---------------
-$(".connection__block").each(function() {
-
-    let more = $(this).find(".connection__block-inner");
-    let hide = $(this).find(".connection__content");
-    hide.hide();
-    more.click(function() {
-        hide.slideToggle();
-        more.toggleClass('active');
-    });
-});
-
-
-$('.connection__item').click(function() {
-    var id = $(this).attr('data-tab'),
-        content = $('.connection__wrap[data-tab="'+ id +'"]');
-    
-    $('.connection__item.active').removeClass('active'); // 1
-    $(this).addClass('active'); // 2
-    
-    $('.connection__wrap.active').removeClass('active'); // 3
-    content.addClass('active'); // 4
-});
-//--------------------------------------
-
-
-//-------------------About------------
-$('.about__item-inteligence').click(function() {
-	var id = $(this).attr('data-tab'),
-			content = $('.inteligence__wrap[data-tab="'+ id +'"]');
-	
-	$('.inteligence__wrap.active').removeClass('active'); // 3
-	content.addClass('active'); // 4
-
-	content.slideToggle();
-});
-//-----------------------------
-
-
-//---------------Distant-------------
-$(".distant__btn-two").click(function() {
-	$(".distant__wrap").slideDown();
-	$(".distant__btn-two").addClass('hidden');
-});
-
-
-$(".distant__back").click(function() {
-	$(".distant__wrap").slideUp();
-	$(".distant__btn-two").removeClass('hidden');
-});
-
-
-$(".distant__btn-two").click(function() {
-	$(".distant__wrap").slideDown();
-	$(".distant__btn-two").addClass('hidden');
-});
-
-
-$(".distant__text-btn").click(function() {
-	$(".distant__text").slideToggle();
-	if($(".distant__text-btn").text() === 'Раскрыть текст') {
-		$(".distant__text-btn").text("Скрыть текст");
-	} else {
-		$(".distant__text-btn").text("Раскрыть текст")
-	}
-});
-//---------------------------------------
-
-
 //----------Header---------------------
 $(".header__bottom-item").hover(function() {
   $(this).toggleClass('active');
@@ -174,7 +57,6 @@ $(".header__bottom-item").hover(function() {
     $(this).children(".header__bottom-down").stop().slideUp(300);
   }
 });
-  
 
 $(".header__center-item").hover(function() {
   $(this).toggleClass('active');
@@ -186,7 +68,6 @@ $(".header__center-item").hover(function() {
   }
 });
 
-	
 $(".header__center-subitem").hover(function() {
   $(this).toggleClass('active');
   var isHovered = $(this).is(":hover");
@@ -199,32 +80,7 @@ $(".header__center-subitem").hover(function() {
 //-------------------------------//
 
 
-//-------------Clinical---------------------//
-$('.clinical-two__tabs-item').click(function() {
-	var id = $(this).attr('data-tab'),
-			content = $('.clinical-two__wrap[data-tab="'+ id +'"]');
-	
-	$('.clinical-two__tabs-item.active').removeClass('active'); // 1
-	$(this).addClass('active'); // 2
-	
-	$('.clinical-two__wrap.active').removeClass('active'); // 3
-	content.addClass('active'); // 4
-});
-//-----------------------------------------//
-
-
-
-
-
-
-$('[data-open-vacancies').on('click', function() {
-	$('[data-open-vacancies]').removeClass('active');
-  $(`[data-open-vacancies="${$(this).data('open-vacancies')}"`).addClass('active');
-
-	$('[data-vacancies]').removeClass('active');
-  $(`[data-vacancies="${$(this).data('open-vacancies')}"`).addClass('active');
-});
-
+//-------------------------About---------------------------------------//
 $('[data-open-menu').on('click', function() {
 	$('[data-open-menu]').removeClass('active');
   $(`[data-open-menu="${$(this).data('open-menu')}"`).addClass('active');
@@ -233,7 +89,15 @@ $('[data-open-menu').on('click', function() {
   $(`[data-menu="${$(this).data('open-menu')}"`).addClass('active');
 	
 	const title = document.querySelector(`[data-open-menu="${$(this).data('open-menu')}"`).firstChild.textContent;
-	$('.title').html(title);
+	$('.about__title').html(title);
+});
+
+$('[data-open-vacancies').on('click', function() {
+	$('[data-open-vacancies]').removeClass('active');
+  $(`[data-open-vacancies="${$(this).data('open-vacancies')}"`).addClass('active');
+
+	$('[data-vacancies]').removeClass('active');
+  $(`[data-vacancies="${$(this).data('open-vacancies')}"`).addClass('active');
 });
 
 $('[data-open-structure').on('click', function() {
@@ -255,7 +119,6 @@ $('[data-open-photo-category').on('click', function() {
   $(`[data-photo-category="${$(this).data('open-photo-category')}"`).addClass('active');
 });
 
-
 $('.photos__item-image').on('click', function() {
 	$('.photos__gallery').addClass('active')
 	$('.photos__contents').removeClass('active')
@@ -263,3 +126,113 @@ $('.photos__item-image').on('click', function() {
 	const title = $(this).find('.photos__item-title').text()
 	$('.photos__gallery-title').text(title);
 })
+//--------------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+$(".cost__block").each(function() {
+    let more = $(this).find(".cost__block-inner");
+    let hide = $(this).find(".cost__content");
+    hide.hide();
+    more.click(function() {
+        hide.slideToggle();
+        more.toggleClass('active');
+    });
+});
+
+$(".schedule__block").each(function() {
+    let more = $(this).find(".schedule__block-inner");
+    let hide = $(this).find(".schedule__wrap");
+    hide.hide();
+    more.click(function() {
+        hide.slideToggle();
+        more.toggleClass('active');
+    });
+});
+
+
+$('.hospitalization__item').click(function() {
+	var id = $(this).attr('data-tab'),
+			content = $('.hospitalization__content[data-tab="'+ id +'"]');
+	
+	$('.hospitalization__item.active').removeClass('active'); // 1
+	$(this).addClass('active'); // 2
+	
+	$('.hospitalization__content.active').removeClass('active'); // 3
+	content.addClass('active'); // 4
+
+	if(id == 1) {
+		$('.hospitalization__block').removeClass('hidden'); // 1
+	} else {
+		$('.hospitalization__block').addClass('hidden'); // 1
+	}
+});
+});
+
+//--------------Connecttion---------------
+$(".connection__block").each(function() {
+
+    let more = $(this).find(".connection__block-inner");
+    let hide = $(this).find(".connection__content");
+    hide.hide();
+    more.click(function() {
+        hide.slideToggle();
+        more.toggleClass('active');
+    });
+});
+
+$('.connection__item').click(function() {
+    var id = $(this).attr('data-tab'),
+        content = $('.connection__wrap[data-tab="'+ id +'"]');
+    
+    $('.connection__item.active').removeClass('active'); // 1
+    $(this).addClass('active'); // 2
+    
+    $('.connection__wrap.active').removeClass('active'); // 3
+    content.addClass('active'); // 4
+});
+//--------------------------------------
+
+//---------------Distant-------------
+$(".distant__btn-two").click(function() {
+	$(".distant__wrap").slideDown();
+	$(".distant__btn-two").addClass('hidden');
+});
+
+$(".distant__back").click(function() {
+	$(".distant__wrap").slideUp();
+	$(".distant__btn-two").removeClass('hidden');
+});
+
+$(".distant__btn-two").click(function() {
+	$(".distant__wrap").slideDown();
+	$(".distant__btn-two").addClass('hidden');
+});
+
+$(".distant__text-btn").click(function() {
+	$(".distant__text").slideToggle();
+	if($(".distant__text-btn").text() === 'Раскрыть текст') {
+		$(".distant__text-btn").text("Скрыть текст");
+	} else {
+		$(".distant__text-btn").text("Раскрыть текст")
+	}
+});
+//---------------------------------------
+
+//-------------Clinical---------------------//
+$('.clinical-two__tabs-item').click(function() {
+	var id = $(this).attr('data-tab'),
+			content = $('.clinical-two__wrap[data-tab="'+ id +'"]');
+	
+	$('.clinical-two__tabs-item.active').removeClass('active'); // 1
+	$(this).addClass('active'); // 2
+	
+	$('.clinical-two__wrap.active').removeClass('active'); // 3
+	content.addClass('active'); // 4
+});
+//-----------------------------------------//
--- app/js/js.js
+++ app/js/js.js
@@ -1,38 +1,3 @@
-//------------Смена активной кнопки----------------
-const distantBtns = document.querySelectorAll('.distant__btn');
-const vacanciesBtns = document.querySelectorAll('.vacancies__menu-item');
-const photosBtns = document.querySelectorAll('.photos__menu-item');
-const clinicalRecBtns = document.querySelectorAll('.clinical-two__menu-item');
-
-
-const setActiveBtn = (indexButton, btns) => {
-	for(let button of btns) {
-		button.classList.remove('active');
-	}
-	btns[indexButton].classList.add('active');
-}
-
-const clickActiveBtn = (item, indexButton, btns) => {
-	item.addEventListener('click', () => {
-		setActiveBtn(indexButton, btns);
-	})
-}
-
-
-const activeBtnDistant = indexButton => setActiveBtn(indexButton, distantBtns)
-distantBtns.forEach((item, indexButton) => clickActiveBtn(item, indexButton, distantBtns))
-
-const activeBtnVacancies = indexButton => setActiveBtn(indexButton, vacanciesBtns)
-vacanciesBtns.forEach((item, indexButton) => clickActiveBtn(item, indexButton, vacanciesBtns))
-
-const activeBtnPhotos = indexButton => setActiveBtn(indexButton, photosBtns)
-photosBtns.forEach((item, indexButton) => clickActiveBtn(item, indexButton, photosBtns))
-
-const activeBtnClinicalRec = indexButton => setActiveBtn(indexButton, clinicalRecBtns)
-clinicalRecBtns.forEach((item, indexButton) => clickActiveBtn(item, indexButton, clinicalRecBtns))
-//-------------------------------------------------//
-
-
 //--------------Modal (conferences)-----------------//
 import { dataConferences } from "./data/dataConferences.js";
 

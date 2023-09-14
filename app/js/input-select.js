d7e604fd2a23eb3d7649a5b1607b074cc103b968
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-09-14 15:18:24 +0300

    update

--- app/js/input-select.js
+++ app/js/input-select.js
@@ -9,76 +9,30 @@ $(document).ready(function () {
     $('.select').on('click', '.select-head', function () {
         if ($(this).hasClass('open')) {
             $(this).removeClass('open');
-            $(this).next().fadeOut();
+            $(this).next().slideUp();
         } else {
             $('.select-head').removeClass('open');
-            $('.select-list').fadeOut();
+            $('.select-list').slideUp();
             $(this).addClass('open');
-            $(this).next().fadeIn();
+            $(this).next().slideDown();
         }
     });
 
     $('.select').on('click', '.select-item', function () {
         $('.select-head').removeClass('open');
-        $(this).parent().fadeOut();
-        $(this).parent().prev().text($(this).text());
-        $(this).parent().prev().prev().val($(this).text());
+				$(this).siblings().removeClass('active')
+				$(this).addClass('active')
+        $(this).parent().parent().slideUp();
+        $(this).parent().parent().prev().text($(this).text());
+        $(this).parent().parent().prev().prev().val($(this).text());
     });
 
     $(document).click(function (e) {
         if (!$(e.target).closest('.select').length) {
             $('.select-head').removeClass('open');
-            $('.select-list').fadeOut();
+            $('.select-list').slideUp();
         }
     });
 });
 
 $('.date').mask('99.99.9999', { placeholder: 'дд.мм.гггг' });
-
-
-
-  function inputDatalist(event) {
-    const targetContainer = $(event.target).closest('.input-datalist-form');
-
-    if (targetContainer) {
-      const inputBlock = targetContainer;
-      const datalist = targetContainer.find('.input-datalist-form__list');
-      const input = targetContainer.find('input');
-      const inputMainBlock = targetContainer.find('.input-datalist-form__block');
-      const option = targetContainer.find('option');
-
-      targetContainer
-        .each(function () {
-          $(this).find('.input-datalist-form__list').not(datalist).slideUp();
-          $(this).find('.input-datalist-form__block').not(inputMainBlock).removeClass('active');
-        });
-
-      datalist.slideToggle();
-      inputMainBlock.toggleClass('active');
-
-      datalist.find('option').each(function () {
-        $(this).on('click', function () {
-          input.val($(this).html());
-          inputBlock.addClass('checked');
-          option.not($(this)).removeClass('active');
-          $(this).addClass('active');
-        });
-      });
-
-      input.on('input', () => {
-        const inputValue = input.val().toLowerCase().trim();
-        option.each(function () {
-          const optionValue = $(this).text().toLowerCase();
-          if (optionValue.indexOf(inputValue) === 0) {
-            $(this).css('display', 'block');
-          } else {
-            $(this).css('display', 'none');
-          }
-        });
-      });
-    }
-  }
-
-  $('.input-datalist-form').on('click', (event) => {
-    inputDatalist(event);
-  });

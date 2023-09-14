ff61ab226561936667b1d8a318de90e0e92e6902
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-09-14 09:53:48 +0300

    feat: add datalist

--- build/js/input-select.js
+++ build/js/input-select.js
@@ -34,3 +34,51 @@ $(document).ready(function () {
 });
 
 $('.date').mask('99.99.9999', { placeholder: 'дд.мм.гггг' });
+
+
+
+  function inputDatalist(event) {
+    const targetContainer = $(event.target).closest('.input-datalist-form');
+
+    if (targetContainer) {
+      const inputBlock = targetContainer;
+      const datalist = targetContainer.find('.input-datalist-form__list');
+      const input = targetContainer.find('input');
+      const inputMainBlock = targetContainer.find('.input-datalist-form__block');
+      const option = targetContainer.find('option');
+
+      targetContainer
+        .each(function () {
+          $(this).find('.input-datalist-form__list').not(datalist).slideUp();
+          $(this).find('.input-datalist-form__block').not(inputMainBlock).removeClass('active');
+        });
+
+      datalist.slideToggle();
+      inputMainBlock.toggleClass('active');
+
+      datalist.find('option').each(function () {
+        $(this).on('click', function () {
+          input.val($(this).html());
+          inputBlock.addClass('checked');
+          option.not($(this)).removeClass('active');
+          $(this).addClass('active');
+        });
+      });
+
+      input.on('input', () => {
+        const inputValue = input.val().toLowerCase().trim();
+        option.each(function () {
+          const optionValue = $(this).text().toLowerCase();
+          if (optionValue.indexOf(inputValue) === 0) {
+            $(this).css('display', 'block');
+          } else {
+            $(this).css('display', 'none');
+          }
+        });
+      });
+    }
+  }
+
+  $('.input-datalist-form').on('click', (event) => {
+    inputDatalist(event);
+  });
